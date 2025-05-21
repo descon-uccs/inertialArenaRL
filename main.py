bccd2af21f6bbaaa90f146c0248af2efd5ad2733
Philip N. Brown <pbrown2@uccs.edu>
2025-05-21 16:03:03 -0600

    driver code doing vids now

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ Created on Wed May 21 10:47:12 2025
 """
 
 from classes import InertialContinuousArena
-from util import uglyVideo
+from util import uglyVideo, record_video
 import matplotlib.pyplot as plt
 import numpy as np
+import gymnasium as gym
 
 from stable_baselines3 import PPO, A2C, DQN
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
 
 if __name__=="__main__" :
+    
+    gym.register('InertialContinuousArena',InertialContinuousArena)
         
     # uglyVideo(10,env)
     
@@ -25,10 +28,13 @@ if __name__=="__main__" :
     
     model = PPO("MlpPolicy", env, verbose=1, device='cpu')
     
-    # uglyVideo(10,10,env,model)
+    record_video('InertialContinuousArena', model,prefix='untrained')
     
     # Train the agent
-    model.learn(total_timesteps=100_000)
+    model.learn(total_timesteps=10_000)
+    record_video('InertialContinuousArena', model,prefix='10ksteps')
+    model.learn(total_timesteps=90_000)
+    record_video('InertialContinuousArena', model,prefix='100ksteps')
     
     
     # uglyVideo(10,10,env,model)
